b1065d5025c8488ea856b003d0a1b8acaa2b26b3
madise1202 <madis@LAPTOP-QOU1RNLG>
2020-02-06 13:06:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserInputMadisonEcrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pause to display my name
+            Console.WriteLine("Hi, This is Madison Ecrement! Let me know how you are doing, type your answer below and hit enter:");
+            string name = Console.ReadLine();
+            Console.WriteLine("You typed: " + name);
+            Console.ReadKey();
+        }
+    }
+}
